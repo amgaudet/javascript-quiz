e2e90b2efe67a077d2e82df4fcaa622adfcdcdff
Shreekz <alanmguadet@gmail.com>
2021-05-20 23:04:38 -0400

    updated questions

--- assets/script.js
+++ assets/script.js
@@ -4,17 +4,14 @@ var answerEl = document.querySelector("#answers");
 var secondsLeft = 60;
 var gameOn = false;
 
-// var questions = ["q1", "q2", "q3", "q4"];
-// var answers = [["a1a", "a1b", "a1c"],["a2a", "a2b", "a2c"],["a3a", "a3b", "a3c"],["a4a", "a4b", "a4c"],];
-
 var questions = {
-    question: ["Question number 1", "Question number 2", "Question number 3", "Question number 4", "Question number 5", "Question number 6"],
-    answers: [["Answer 1a", "Answer 1b", "Answer 1c", "Answer 1d"], ["Answer 2a", "Answer 2b", "Answer 2c", "Answer 2d"], ["Answer 3a", "Answer 3b", "Answer 3c", "Answer 3d"], ["Answer 4a", "Answer 4b", "Answer 4c", "Answer 4d"], ["Answer 5a", "Answer 5b", "Answer 5c", "Answer 5d"], ["Answer 6a", "Answer 6b", "Answer 6c", "Answer 6d"]]
+    question: ["What does HTML stand for?", "What is the correct syntax in HTML for a paragraph element?", "What does CSS stand for?", "What is the correct syntax to define an array in javascript?", "Where is the correct place to insert CSS in your HTML document?", "Which of these will define a function in javascript?"],
+    answers: [["Hypertext Markup Language", "Hyperlink Text Mixer Language", "Hit The Millions", "How To Make Lists"], ["<p>", "<paragraph>", "[p]", "<Im making a new paragraph here>"], ["Cascading Style Sheets", "Cheap Shot Sucka!!", "Current Style Syntax", "Communication Styling Services"], ["var x = [];", "var x = {};", "var x = []", "var x = this is an array."], ["In the head", "Inline, always", "At the bottom of the body", "Inject with a needle"], ["function fun()", "fun() the function", "fun() var", "fun() I'm making a function here!"]]
 }
 
 //loads initial screen, runs on init
 function setStarterScreen(){
-    var start = document.createElement("button")
+    var start = document.createElement("button");
     start.textContent = "Start Quiz";
     questionEl.textContent = "Welcome to the Javascript quiz. Select the correct answer to each question. Wrong answers subrats 10 seconds from the timer. You've lost when the time is up";
     answerEl.appendChild(start);
@@ -37,8 +34,21 @@ function setTimer(){
 function gameOver(){
     //check time left on the clock
     var score = secondsLeft;
-
+    var submission = document.createElement("form");
+    var name = document.createElement("textarea");
+    var submit = document.createElement("button");
+    submit.setAttribute("class", "submitter");
+    var prompt = document.createElement("p");
+    
     questionEl.textContent = "Your score is: " + score;
+    questionEl.appendChild(submission);
+    submission.appendChild(prompt);
+    submission.appendChild(name);
+    submission.appendChild(submit);
+    prompt.textContent = "Enter Initials: ";
+    submit.textContent = "Submit!";
+
+
     //display time left as score
     //Enter name/initials for high score -> local storage
     //link to high scores
@@ -46,41 +56,42 @@ function gameOver(){
 
 //populate question/answers
 function gameStart(){
+    // stopClock();
     answerEl.textContent = "";
     //random question selector
     randomSelector = Math.floor(Math.random() * questions.question.length);
     //update question-title with question
-    questionEl.textContent = questions.question[randomSelector];
-    var answerChoices = questions.answers[randomSelector];
-    console.log(typeof answerChoices);
-    //populate possible answers
-    for (var i = 0; i < 4; i++){
-        //append new list item
-        var option = document.createElement("button");
-        //apply answer content
-        option.textContent = answerChoices[i];
-        answerEl.appendChild(option);
-
-    }
-
-    //removes selected question/answer from arrays
-    questions.question.splice(randomSelector,1);
-    questions.answers.splice(randomSelector,1);
-    
-    if (questions.length === 0){
+    if (questions.question.length === 0){
         gameOver();
+    } else {
+        questionEl.textContent = questions.question[randomSelector];
+        var answerChoices = questions.answers[randomSelector];
+        console.log(typeof answerChoices);
+        //populate possible answers
+        for (var i = 0; i < 4; i++){
+            //append new list item
+            var option = document.createElement("button");
+            option.setAttribute("class", "answer-choice");
+            //apply answer content
+            option.textContent = answerChoices[i];
+            answerEl.appendChild(option);
+        
+        }       
+        //removes selected question/answer from arrays
+        questions.question.splice(randomSelector,1);
+        questions.answers.splice(randomSelector,1);
     }
-
 }
 
-answerEl.addEventListener("click", function(){
-    if (!gameOn){
-        gameOn = true;
-        setTimer();
-        gameStart();
-    } else {
-        gameStart();
-    }
+
+answerEl.addEventListener("click", function(event){
+        if (!gameOn){
+            gameOn = true;
+            setTimer();
+            gameStart();
+        } else  {
+            gameStart();
+        }
 });
 
 
